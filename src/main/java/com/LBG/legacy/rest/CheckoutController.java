57d6fd6de4472222a45815abb4100bf5e114d6cb
liammcnabola <liam.mcnabola@protonmail.com>
2024-03-25 14:01:04 +0000

    added CrossOrigin to CheckoutController

--- src/main/java/com/LBG/legacy/rest/CheckoutController.java
+++ src/main/java/com/LBG/legacy/rest/CheckoutController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,7 @@ import com.LBG.legacy.domain.Checkout;
 import com.LBG.legacy.services.CheckoutService;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/checkout")
 public class CheckoutController {
 
